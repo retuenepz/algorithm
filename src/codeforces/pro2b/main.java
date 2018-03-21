c40a3213aad7d21071b7ea8a2973ecfc8f7f4a73
hongyb <734703533@qq.com>
2018-03-21 14:51:05 +0800

    :tada: 噗噗噗

---
+++ src/codeforces/pro2b/main.java
@@ -0,0 +1,10 @@
+package codeforces.pro2b;
+
+/**
+ * 作者:hongyanbo
+ * 时间:2018/3/20
+ * 这题不会做，看都看不懂。shit!
+ */
+public class main {
+
+}
--- src/codeforces/test/Test.java
+++ src/codeforces/test/Test.java
@@ -1,13 +1,31 @@
 package codeforces.test;
 
+import java.util.Scanner;
+
 /**
+ * 公共子序列。
  * 作者:hongyanbo
  * 时间:2018/3/20
  */
 public class Test {
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
-            System.out.println(i);
+        Scanner scanner = new Scanner(System.in);
+        String s1 = scanner.next();
+        String s2 = scanner.next();
+
+
+        System.out.println(maxLen(s1,s2));
+    }
+
+    private static int maxLen(String s1, String s2) {
+        if(s1.length()>0 && s2.length()>0){
+            if(s1.charAt(0)==s2.charAt(0)){
+                return maxLen(s1.substring(1,s1.length()),s2.substring(1,s2.length()))+1;
+            }else{
+                return Math.max(maxLen(s1.substring(1,s1.length()),s2),maxLen(s1,s2.substring(1,s2.length())));
+            }
+        }else{
+            return 0;
         }
     }
 }
